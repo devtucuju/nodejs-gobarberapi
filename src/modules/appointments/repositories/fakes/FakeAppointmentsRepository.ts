cca8517cc3333d91cdd3645ec6f562a2f7c7db8a
devtucuju <jsdeveloperweb@gmail.com>
2021-03-07 18:31:53 -0300

    :white_check_mark: impedir criacao de agendamento mesmo horario

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,21 +1,17 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmnetsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '../../infra/typeorm/entities/Appointment';
-
-// interface CreateAppointmentDTO {
-//   provider: string;
-//   date: Date;
-// }
 
 class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const appointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
-    return appointment;
+    return findAppointment;
   }
 
   public async create({
